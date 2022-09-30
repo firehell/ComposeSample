cfbdaf5bdd2a1c0b688f5071469f44cea785ff01
zz <13188961863@163.com>
2022-09-30 14:04:55 +0800

    底部栏功能完成

--- app/src/main/java/com/zz/composesample/MainActivity.kt
+++ app/src/main/java/com/zz/composesample/MainActivity.kt
@@ -6,31 +6,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
 import com.zz.composesample.ui.WeChatBottomBar
 import com.zz.composesample.ui.theme.WeComposeTheme
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalPagerApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WeComposeTheme() {
-                Column {
-                  WeChatBottomBar(onStateChange = {
-
-                  })
-                }
+            WeComposeTheme(WeComposeTheme.Theme.Light) {
+                var selectedTab by remember { mutableStateOf(0) }
+                WeChatBottomBar(selectedTab) { selectedTab = it }
             }
         }
     }
--- app/src/main/java/com/zz/composesample/ui/WeChatBottomBar.kt
+++ app/src/main/java/com/zz/composesample/ui/WeChatBottomBar.kt
@@ -1,6 +1,7 @@
 package com.zz.composesample.ui
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,30 +9,51 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.zz.composesample.R
 import com.zz.composesample.ui.theme.WeComposeTheme
-
 
 @Preview(showBackground = true)
 @Composable
 fun WeBottomBarPreview() {
     WeComposeTheme(WeComposeTheme.Theme.Light) {
-        WeChatBottomBar() {
-        }
+        var selectedTab by remember { mutableStateOf(0) }
+        WeChatBottomBar(selectedTab) { selectedTab = it }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WeBottomBarPreviewDark() {
+    WeComposeTheme(WeComposeTheme.Theme.Dark) {
+        var selectedTab by remember { mutableStateOf(0) }
+        WeChatBottomBar(selectedTab) { selectedTab = it }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WeBottomBarPreviewNewYear() {
+    WeComposeTheme(WeComposeTheme.Theme.NewYear) {
+        var selectedTab by remember { mutableStateOf(0) }
+        WeChatBottomBar(selectedTab) { selectedTab = it }
     }
 }
 
 @Composable
-fun WeChatBottomBar(onStateChange: (Int) -> Unit) {
-    Row() {
+fun WeChatBottomBar(selectedIndex: Int, onStateChange: (Int) -> Unit) {
+    Row(Modifier.background(WeComposeTheme.colors.bottomBar)) {
         TabItem(
-            R.drawable.ic_chat_outlined, "聊天",
+            if (selectedIndex == 0) R.drawable.ic_chat_filled else R.drawable.ic_chat_outlined,
+            "聊天",
+            if (selectedIndex == 0) WeComposeTheme.colors.iconCurrent else WeComposeTheme.colors.icon,
             Modifier
                 .weight(1.0f)
                 .clickable {
@@ -39,7 +61,9 @@ fun WeChatBottomBar(onStateChange: (Int) -> Unit) {
                 }
         )
         TabItem(
-            R.drawable.ic_contacts_outlined, "通讯录",
+            if (selectedIndex == 1) R.drawable.ic_contacts_filled else R.drawable.ic_contacts_outlined,
+            "通讯录",
+            if (selectedIndex == 1) WeComposeTheme.colors.iconCurrent else WeComposeTheme.colors.icon,
             Modifier
                 .weight(1.0f)
                 .clickable {
@@ -47,7 +71,9 @@ fun WeChatBottomBar(onStateChange: (Int) -> Unit) {
                 }
         )
         TabItem(
-            R.drawable.ic_discovery_outlined, "发现",
+            if (selectedIndex == 2) R.drawable.ic_discovery_filled else R.drawable.ic_discovery_outlined,
+            "发现",
+            if (selectedIndex == 2) WeComposeTheme.colors.iconCurrent else WeComposeTheme.colors.icon,
             Modifier
                 .weight(1.0f)
                 .clickable {
@@ -55,7 +81,9 @@ fun WeChatBottomBar(onStateChange: (Int) -> Unit) {
                 }
         )
         TabItem(
-            R.drawable.ic_me_outlined, "我",
+            if (selectedIndex == 3) R.drawable.ic_me_filled else R.drawable.ic_me_outlined,
+            "我",
+            if (selectedIndex == 3) WeComposeTheme.colors.iconCurrent else WeComposeTheme.colors.icon,
             Modifier
                 .weight(1.0f)
                 .clickable {
@@ -66,15 +94,12 @@ fun WeChatBottomBar(onStateChange: (Int) -> Unit) {
 }
 
 @Composable
-fun TabItem(@DrawableRes iconId: Int, title: String, modifier: Modifier = Modifier) {
+fun TabItem(@DrawableRes iconId: Int, title: String, tint: Color, modifier: Modifier = Modifier) {
     Column(
-        modifier.padding(8.dp),
+        modifier.padding(vertical = 8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Icon(
-            painterResource(id = iconId), contentDescription = title,
-            Modifier.size(24.dp)
-        )
-        Text(text = title)
+        Icon(painterResource(iconId), title, Modifier.size(24.dp), tint)
+        Text(text = title, fontSize = 20.sp, color = tint)
     }
 }
